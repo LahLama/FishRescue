682617fa4a994e414cb7c7b6a2242dcb449896a1
LahLama <jamesihlenfeldt1@gmail.com>
2026-03-28 18:52:35 +0200

    Feat : Braai Model

--- Assets/Scripts/POS/ShakePOS.cs
+++ Assets/Scripts/POS/ShakePOS.cs
@@ -4,11 +4,11 @@ public class ShakePOS : MonoBehaviour
 {
     void Update()
     {
-        this.transform.localRotation = Quaternion.Euler(0, 0, Mathf.Sin(Time.time * 10) * 5);
+        this.transform.localRotation = Quaternion.Euler(this.transform.localRotation.x, 90, Mathf.Sin(Time.time * 10) * 5);
     }
 
     void OnDisable()
     {
-        this.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        this.transform.localRotation = Quaternion.Euler(this.transform.localRotation.x, 90, 0);
     }
 }
